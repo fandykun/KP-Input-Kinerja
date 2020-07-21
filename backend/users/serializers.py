0dab9a5ef3756b05c9d1b0cff4bf73d0f084a0f3
fandykun <fandy19061999@gmail.com>
2020-07-21 13:33:56 +0700

    remove user apps (unused)

--- backend/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from djoser.serializers import UserCreateSerializer, UserSerializer
-from rest_framework import serializers
-from .models import CustomUser
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = CustomUser
-        fields = ['id', 'nrp', 'password']
