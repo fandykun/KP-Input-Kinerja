168bd9bfe56c13aad56424214ae1dbe3829e4a2c
fandykun <fandy19061999@gmail.com>
2020-08-27 13:40:05 +0700

    add test for debugging

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import TestView
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='Token Authentication API')
+    path('login/', obtain_auth_token, name='Token Authentication API'),
+    path('testing-response/', TestView.as_view())
 ]
--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -5,10 +5,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated  # <-- Here
 
+# from .models import User
+
+# from .serializers import UserMasterSerializer
 
 class HelloView(APIView):
     # permission_classes = (IsAuthenticated,)             # <-- And here
 
     def get(self, request):
+        # user = User.objects.all()
+        # serializer = UserMasterSerializer(user, many=True)
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+class TestView(APIView):
+    def post(self, request):
+        return Response(data=request.data)
