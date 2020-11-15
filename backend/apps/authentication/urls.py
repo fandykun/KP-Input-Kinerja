6718867969608de8651d6ca2a139d0a428506240
fandykun <fandy19061999@gmail.com>
2020-11-15 10:48:01 +0700

    add is_admin key on login response

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import custom_auth_token
 from .views import UserInfoView
 from .views import AllNonValidateView
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='Token Authentication API'),
+    path('login/', custom_auth_token, name='Token Authentication API'),
     path('account/info/', UserInfoView.as_view()),
     path('submission/', AllNonValidateView.as_view())
 ]
