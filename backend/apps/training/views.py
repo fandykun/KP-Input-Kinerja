7f436de9e7e432e82a3f5838ad1e4464101ffb80
arifdarma <arifdarmaalthia@gmail.com>
2020-11-15 12:11:40 +0700

    tambah admin permission training

--- backend/apps/training/views.py
+++ backend/apps/training/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Training
 from .serializers import TrainingSerializer
+from ..authentication.permissions import isAdminPermission
 
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
@@ -10,6 +12,8 @@ from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
 
 from url_filter.integrations.drf import DjangoFilterBackend
 # from django_filters.rest_framework import DjangoFilterBackend
@@ -61,6 +65,18 @@ class TrainingDetailsAPIView(APIView):
         train_dosen = self.get_object(pk)
         train_dosen.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Validated API
+@api_view(['POST'])
+@permission_classes([isAdminPermission])
+def set_validate(request, pk):
+    if request.method == 'POST':
+        queryset = Training.objects.all()
+        train = get_object_or_404(queryset, pk=pk)
+        if not kultam.is_validated:
+            train.is_validated = True
+            train.save()
+        return Response(status=status.HTTP_200_OK)
 
 class TrainingList(ListAPIView):
     permission_classes = (IsAuthenticated, )
